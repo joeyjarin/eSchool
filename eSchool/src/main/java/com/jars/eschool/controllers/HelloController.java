b7e80b71a1cf419121a37bb26da94649ed8657b4
joey <joeyjarin@yahoo.com>
2016-09-25 11:22:25 +0800

    Added /jsontest

--- eSchool/src/main/java/com/jars/eschool/controllers/HelloController.java
+++ eSchool/src/main/java/com/jars/eschool/controllers/HelloController.java
@@ -1,5 +1,9 @@
 package com.jars.eschool.controllers;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,7 +12,15 @@ public class HelloController {
 
     @RequestMapping("/")
     public String index() {
-        return "Greetings from Spring Boot!";
+        return "Greetings from JARS eSchool!";
     }
 
+    @RequestMapping("/jsontest") 
+    public @ResponseBody Map<String, String> callSomething () {
+        
+        Map<String, String> map = new HashMap<String, String>();
+        map.put("url", "http://www.jars.com");
+        
+        return map;
+    }    
 }
